9afaa0b40ad15940039dd226ebe07ec0f730d9ff
Heather Iskander <4367909+heatheriskander@users.noreply.github.com>
2026-04-21 18:03:48 -0400

    Add toggle to fix conflict with default keybinds

--- nabber.lua
+++ nabber.lua
@@ -5,6 +5,8 @@ local input = require("mp.input")
 local out_dir = mp.command_native({ "expand-path", "~/clips" })
 
 local start_time, end_time
+local nabber_enabled = false
+
 local function nab()
 	if not start_time or not end_time then
 		mp.osd_message("set start and end points first", 3)
@@ -61,12 +63,26 @@ local function nab()
 	})
 end
 
-mp.add_key_binding("i", "set-start", function()
-	start_time = mp.get_property_number("time-pos")
-	mp.osd_message("clip start: " .. start_time, 3)
-end)
-mp.add_key_binding("o", "set-end", function()
-	end_time = mp.get_property_number("time-pos")
-	mp.osd_message("clip end: " .. end_time, 3)
-end)
-mp.add_key_binding("p", "nab", nab)
+local function toggle_nabber()
+	if not nabber_enabled then
+		mp.add_forced_key_binding("i", "set-start", function()
+			start_time = mp.get_property_number("time-pos")
+			mp.osd_message("clip start: " .. start_time, 3)
+		end)
+		mp.add_forced_key_binding("o", "set-end", function()
+			end_time = mp.get_property_number("time-pos")
+			mp.osd_message("clip end: " .. end_time, 3)
+		end)
+		mp.add_forced_key_binding("p", "nab", nab)
+		nabber_enabled = true
+		mp.osd_message("nabber: enabled")
+	else
+		mp.remove_key_binding("set-start")
+		mp.remove_key_binding("set-end")
+		mp.remove_key_binding("nab")
+		nabber_enabled = false
+		mp.osd_message("nabber: disabled")
+	end
+end
+
+mp.add_key_binding("n", "toggle-nabber", toggle_nabber)
